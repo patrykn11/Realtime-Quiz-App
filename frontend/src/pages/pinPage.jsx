8a264725d5a39dc45b7834890d03faf5bc50c1e4
Patryk Nisgorski <patryknisgorski@gmail.com>
2026-04-30 03:28:28 +0200

    fetching quizes name in frontend

--- frontend/src/pages/pinPage.jsx
+++ frontend/src/pages/pinPage.jsx
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {useNavigate} from "react-router-dom";
 
 
 export default function PinPage() {
   const [code, setCode] = useState("0000");
   const navigate = useNavigate();
+  const [quizes, setQuizes] = useState([]);
+
   const join_room = async () =>{
     navigate(`/LobbyRoom/${code}`);
   }
+
+  // Niesprawdzone
+  // useEffect(() =>{
+  //   const fetch_quizes = async () =>{
+  //   const res = await fetch(`${API_URL}/api/quizes_name/`, {
+  //     method:"GET",
+  //     })
+  //   const data = await res.json()
+  //   setQuizes(data)
+  //   }
+  //   fetch_quizes()
+    
+    
+
+  // }, [])
   return (
     
       <div className="absolute top-[30vh] left-1/2 -translate-x-1/2 flex flex-col items-center justify-center text-white text-xl p-1 gap-10 w-full max-w-md">
